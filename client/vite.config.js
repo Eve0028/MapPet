b0e85f8fb5e02e58832fef01d31f10bc18ab7bdf
Eve0028 <sylwia14082@gmail.com>
2023-03-19 23:43:07 +0100

    Fix: client - add configuration for docker

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  server: {    // <-- this object is added
+    host: true,
+    port: 8080,
+    // https: true,
+    hmr:{
+      clientPort: 443,
+    },
+  },
+  plugins: [vue()]
 })
