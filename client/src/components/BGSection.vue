b21b212e976796ea9944fb7881040188cfa4b2c4
Eve0028 <sylwia14082@gmail.com>
2023-03-25 14:13:33 +0100

    Chore: fill BGSection component
    Template and styles

--- client/src/components/BGSection.vue
+++ client/src/components/BGSection.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
+  <section class="background">
+    <slot></slot>
+  </section>
 </template>
 
 <style scoped>
+  .background{
+    background: #FFFFFF;
+    box-shadow: 1px 1px 6px 2px rgba(0, 0, 0, 0.25);
+    padding: 2em 3em;
+    margin: 2em;
+  }
 </style>
