ac337e87161a173d24144c934d0a4b70d99c0374
Eve0028 <sylwia14082@gmail.com>
2023-03-25 14:12:43 +0100

    Chore: fill buttons components - basic code
    Added props, template and styles to buttons components:
    Button, FindButton, LostButton, MappetButton.

--- client/src/components/buttons/Button.vue
+++ client/src/components/buttons/Button.vue
@@ -1,8 +1,18 @@
 <script setup>
+const props = defineProps(['buttonText'])
 </script>
 
 <template>
+  <button class="button">{{ props.buttonText }}</button>
 </template>
 
 <style scoped>
+.button {
+  padding: 0.8em 1.5em;
+  font-family: 'Fira Sans', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 2em;
+  text-transform: uppercase;
+}
 </style>
--- client/src/components/buttons/MappetButton.vue
+++ client/src/components/buttons/MappetButton.vue
@@ -1,8 +1,17 @@
 <script setup>
+import Button from './Button.vue'
+
+const buttonText = 'view mappet'
 </script>
 
 <template>
+  <Button class="mappet-button" :button-text=buttonText />
 </template>
 
 <style scoped>
+.mappet-button {
+  border: 2px solid rgba(151, 175, 0, 1);
+  background-color: #ffffff;
+  color: rgba(151, 175, 0, 1);
+}
 </style>
