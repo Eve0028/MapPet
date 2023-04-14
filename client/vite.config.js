ad7f6f843ded79d4f468cc303e4b7397193199fc
Eve0028 <sylwia14082@gmail.com>
2023-04-14 14:12:56 +0200

    Fix: client hot reloading
    And update node images version

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   server: {
     host: true,
     port: 8080,
+
+    hmr: {
+      host: 'localhost',
+    },
+    watch: {
+      usePolling: true
+    }
   },
   plugins: [vue()],
 
