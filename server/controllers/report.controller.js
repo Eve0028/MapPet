faa048f8557a9aeca2b841dcf585f21c41293baf
Eve0028 <sylwia14082@gmail.com>
2023-05-07 14:39:30 +0200

    Add: all requests body to report controller

--- server/controllers/report.controller.js
+++ server/controllers/report.controller.js
@@ -65,25 +65,95 @@ exports.findAll = (req, res) => {
 
 // Find a single Report with an id
 exports.findOne = (req, res) => {
+  const id = req.params.id;
 
+  Report.findById(id)
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found Report with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving Report with id=" + id });
+    });
 };
 
 // Update a Report by the id in the request
 exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
 
+  const id = req.params.id;
+
+  Report.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update Report with id=${id}. Maybe Report was not found!`
+        });
+      } else res.send({ message: "Report was updated successfully." });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Report with id=" + id
+      });
+    });
 };
 
 // Delete a Report with the specified id in the request
 exports.delete = (req, res) => {
+  const id = req.params.id;
 
+  Report.findByIdAndRemove(id)
+    .then(data => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete Report with id=${id}. Maybe Report was not found!`
+        });
+      } else {
+        res.send({
+          message: "Report was deleted successfully!"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Report with id=" + id
+      });
+    });
 };
 
 // Delete all Reports from the database.
 exports.deleteAll = (req, res) => {
-
+  Report.deleteMany({})
+    .then(data => {
+      res.send({
+        message: `${data.deletedCount} Report were deleted successfully!`
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while removing all reports."
+      });
+    });
 };
 
 // Find all published Reports
 exports.findAllPublished = (req, res) => {
-
+  Report.find({ published: true })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving reports."
+      });
+    });
 };
