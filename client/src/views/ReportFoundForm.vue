aed18eb44fcf4939fc8cef1f426be040c57b9f38
Eve0028 <sylwia14082@gmail.com>
2023-05-07 19:50:14 +0200

    Chore: complete form to add pet-found-report
    Make ReportFoundForm work - in basic version.

--- client/src/views/ReportFoundForm.vue
+++ client/src/views/ReportFoundForm.vue
@@ -1,20 +1,69 @@
 <script setup>
-
-import LostButton from "../components/buttons/LostButton.vue";
 import ShareButton from "../components/buttons/ShareButton.vue";
 import FindButton from "../components/buttons/FindButton.vue";
+import { reactive, ref } from "vue";
+import ReportDataService from "../services/ReportDataService.js";
+
+const initialState = {
+  id: "",
+  publisherName: "",
+  ownerName: "",
+  phoneNumber: "",
+  emailAddress: "",
+  imageUrl: "",
+  petType: "",
+  petName: "",
+  microchip: "",
+  registrationNumber: "",
+  reportType: "lost",
+  lastSeen: "",
+  timeOfReport: "",
+  timeOfLastSeen: "",
+  details: "",
+  published: "true",
+}
+
+const petData = reactive({ ...initialState });
+
+function resetForm() {
+  Object.assign(petData, initialState);
+}
+
+const submitted = ref(false)
+
+function saveReport() {
+  ReportDataService.create(petData)
+      .then(response => {
+        petData.id = response.data.id;
+        console.log(response.data);
+        submitted.value = true;
+      })
+      .catch(e => {
+        console.log(e);
+      });
+}
+
+function newReport() {
+  submitted.value = false;
+  resetForm();
+}
+
 </script>
 
 <template>
   <h1 class="report-found-header">Report a found pet</h1>
-  <section class="found-form">
+  <section class="found-form" v-if="!submitted">
     <section class="your-details form-column">
       <h3 class="report-found-your-details">Your details</h3>
       <form class="form-vertical">
-        <input name="your-name" type="text" required placeholder="Your name*">
-        <input name="owner-name" type="text" placeholder="Owner's name">
-        <input name="phone" type="tel" placeholder="Phone number">
-        <input name="email" type="email" required placeholder="Email address*">
+        <input name="your-name" type="text" required placeholder="Your name*"
+               v-model.trim="petData.publisherName">
+        <input name="owner-name" type="text" placeholder="Owner's name"
+               v-model.trim="petData.ownerName">
+        <input name="phone" type="tel" placeholder="Phone number"
+               v-model.trim="petData.phoneNumber">
+        <input name="email" type="email" required placeholder="Email address*"
+               v-model.trim="petData.emailAddress">
       </form>
       <div class="photo-upload">
         <img src=""/> upload a photo of found pet
@@ -25,20 +74,27 @@ import FindButton from "../components/buttons/FindButton.vue";
     <section class="your-details form-column">
       <h3 class="report-found-your-details">Found pet's details</h3>
       <form class="form-vertical">
-        <input name="pet-name" type="text" required placeholder="Pet's name*">
-        <input name="microchip" type="text" placeholder="Microchip number">
-        <input name="registration-number" type="tel" placeholder="Registration number">
+        <input name="pet-name" type="text" required placeholder="Pet's name*"
+               v-model.trim="petData.petName">
+        <input name="microchip" type="text" placeholder="Microchip number"
+               v-model.trim="petData.microchip">
+        <input name="registration-number" type="tel" placeholder="Registration number"
+               v-model.trim="petData.registrationNumber">
 
         <div class="form-radio">
-          <label class="form-control"><input type="radio" name="pet-type" value="dog" />Dog</label>
-          <label class="form-control"><input type="radio" name="pet-type" value="cat" />Cat</label>
-          <label class="form-control"><input type="radio" name="pet-type" value="other" />Other pet</label>
+          <label class="form-control"><input type="radio" name="pet-type" value="dog"
+                                             v-model="petData.petType"/>Dog</label>
+          <label class="form-control"><input type="radio" name="pet-type" value="cat"
+                                             v-model="petData.petType"/>Cat</label>
+          <label class="form-control"><input type="radio" name="pet-type" value="other"
+                                             v-model="petData.petType"/>Other pet</label>
         </div>
 
         <div>
           <p>Where did you last seen found pet?</p>
           <div class="inline found-place">
-            <input name="where-found" type="text" required placeholder="Last place of seen">
+            <input name="where-lost" type="text" required placeholder="Last place of seen"
+                   v-model="petData.lastSeen">
           </div>
         </div>
 
@@ -49,13 +105,18 @@ import FindButton from "../components/buttons/FindButton.vue";
             <input name="when-found-time" type="time" required placeholder="When was it?">
           </div>
         </div>
-        <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Other details about pet found"/>
+        <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Other details about pet found"
+                  v-model="petData.details"/>
       </form>
     </section>
   </section>
+  <div v-else>
+    <h4>You submitted successfully!</h4>
+    <button class="missing-button add-report" @click="newReport">Report another pet</button>
+  </div>
 
-  <section class="form-found-buttons">
-    <FindButton class="found-button"/>
+  <section class="form-found-buttons" v-if="!submitted">
+    <FindButton class="found-button" @click="saveReport"/>
     <ShareButton class="share-button"/>
   </section>
 </template>
@@ -110,5 +171,11 @@ h3{
     flex-grow: 1;
     width: 100%;
   }
+}
+
+.add-report{
+  color: white;
+  background-color: $main-color-found;
+  margin-top: 1em;
 }
 </style>
